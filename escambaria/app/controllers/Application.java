f306f6246b7ce5ee9519ad38a69fe39305cc71e9
unknown <Suporte@LAB02-10.cttmaxwell.intra>
2015-09-03 17:29:15 -0300

    first version of the server

--- escambaria/app/controllers/Application.java
+++ escambaria/app/controllers/Application.java
@@ -1,14 +1,29 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.codehaus.jackson.node.ObjectNode;
+
+import play.*;
+import play.libs.Json;
+import play.mvc.*;
 import views.html.*;
 
+import br.com.escambaria.model.Item;
+
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result getItems() {
+		ObjectNode result = Json.newObject();
+		result.get("{\"items\": [{\"id\":1, \"desc\": \"teste\", \"type\": \"image\", \"url\": \"url\"}] }");
+	    return ok(result);
+	}
+
 }
